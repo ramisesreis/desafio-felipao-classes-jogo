f769cf0dbb7956a3fd71331ee660eb21fbe9f0bd
ramisesreis <ramisesreis@outlook.com>
2024-10-01 10:13:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Classe com informações do Herói
+class hero{
+    constructor (name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+
+    }
+    atack(){
+        let atack
+    }
+}
